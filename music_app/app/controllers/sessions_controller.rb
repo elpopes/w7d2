e0cdec492c3169e6931e86711285aa1df3872620
lorenzo <lorenzotijerina@gmail.com>
2022-12-20 15:09:57 -0500

    complete session controller

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+    before_action :require_logged_out, only: [:new, :create]
+    before_action :require_logged_in, only: [:destory]
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        email = params[:user][:email]
+        password = params[:user][:password]
+        @user = User.find_by_credentials(email, password)
+        if @user
+             login(@user) 
+             redirect_to user_url(@user)
+        else
+            @user = User.new(email: email, password: password)
+            flash.new[:errors] = ["invalid credentials. please try again"]
+            render :new
+        end
+    end
+
+    def destroy
+        logout!
+        flash[:messages] = ["successfully logged out!"]
+        redirect_to new_session_url
+    end
+    
+
 end
--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   before_validation :ensure_session_token
-  validates :username, :session_token, presence: true, uniqueness: true
+  validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   attr_reader :password
 
-  def self.find_by_credentials(username, password)
-    user = User.find_by(username: username)
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
 
     user&.is_password?(password) ? user : nil
   end
