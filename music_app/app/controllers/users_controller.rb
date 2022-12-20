b4c35839a99c1b8f6cfb963c7ade0492efd60409
lorenzo <lorenzotijerina@gmail.com>
2022-12-20 16:42:18 -0500

    fix user show

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -13,13 +13,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save!
       login(@user)
-      redirect_to user_url # TODO: redirect user to bands showpage
+      redirect_to user_url(@user) # TODO: redirect user to bands showpage
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
   end
 
   private
